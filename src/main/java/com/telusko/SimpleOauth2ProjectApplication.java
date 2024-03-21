51ffaf6056e7efad71639d0e70487d2d93beb973
Gaurav Sharma <gsharma1154@gmail.com>
2024-03-22 00:08:12 +0530

    basic project

---
+++ src/main/java/com/telusko/SimpleOauth2ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.telusko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleOauth2ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleOauth2ProjectApplication.class, args);
+    }
+
+}
